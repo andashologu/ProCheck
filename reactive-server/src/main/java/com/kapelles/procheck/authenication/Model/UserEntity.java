a536255a5dea2e520e07ddf61280cb8a78e2a42e
Anda Shologu <SHOLOGUA@cput.ac.za>
2023-08-24 15:15:53 +0200

    authentication methods added

---
+++ reactive-server/src/main/java/com/kapelles/procheck/authenication/Model/UserEntity.java
@@ -0,0 +1,36 @@
+package com.kapelles.procheck.authenication.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("users")
+public class UserEntity {
+
+    @Id
+    @Column("id")
+    private Long id;
+
+    @Column("firstname")
+    private String firstname;
+
+    @Column("lastname")
+    private String lastname;
+
+    @Column("email")
+    private String email;
+
+    @Column("username")
+    private String username;
+}
